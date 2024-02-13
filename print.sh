79668100fcf5a1a7d5945b4ef695d5285f65e451
msree <msreeni2656@gmail.com>
2024-02-13 12:51:31 +0530

    echo command

--- print.sh
+++ print.sh
@@ -1,1 +1,16 @@
 echo hello world
+# red 31
+# green 32
+# yellow 33
+# blue 34
+# magenta 35
+# cyan 36
+
+# syntax
+# echo -e " \e[CODEmMESSAGE\e[0m
+echo -e "\e[31mhelloworld\e[0m"
+echo -e "\e[32mhelloworld\e[0m"
+echo -e "\e[33mhelloworld\e[0m"
+echo -e "\e[34mhelloworld\e[0m"
+echo -e "\e[35mhelloworld\e[0m"
+echo -e "\e[36mhelloworld\e[0m"
